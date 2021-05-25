83b01c0413c1959652fa4c4575b06708890823fa
jhakal.surath@gmail.com <jhakal.surath@gmail.com>
2021-05-25 17:47:13 +0530

    nothing much

--- pages/YourBlog.js
+++ pages/YourBlog.js
@@ -11,33 +11,41 @@ import { useEffect, useState } from "react";
 function YourBlog() {
   const [blogs, setBlogs] = useState([]);
   const [user] = useAuthState(auth);
-  db.collection("posts")
-    .orderBy("likes", "desc")
-    .where("email", "==", "jhakal.surath@gmail.com")
-    .onSnapshot((snapshot) => {
-      setBlogs(snapshot.docs.map((doc) => [doc.id, doc.data()]));
-    });
   const router = useRouter();
-  console.log(blogs);
+  if (user) {
+    db.collection("posts")
+      .orderBy("likes", "desc")
+      .where("email", "==", user.email)
+      .onSnapshot((snapshot) => {
+        setBlogs(snapshot.docs.map((doc) => [doc.id, doc.data()]));
+      });
+    console.log(blogs);
+  }
 
   return (
-    <div>
-      <Head>
-        <title>My Blog</title>
-        <meta name="description" content="My Blog Site!!!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      {user ? (
+        <div>
+          <Head>
+            <title>My Blog</title>
+            <meta name="description" content="My Blog Site!!!" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-      <div className={styles.myBlog_header}>
-        <Header />
-        <div className={styles.myBlog}>
-          <h1 className={styles.blogTitle}>My Blogs</h1>
-          {blogs?.map((blog) => (
-            <Post post={blog[1]} key={blog[0]} id={blog[0]} blog />
-          ))}
+          <div className={styles.myBlog_header}>
+            <Header />
+            <div className={styles.myBlog}>
+              <h1 className={styles.blogTitle}>My Blogs</h1>
+              {blogs?.map((blog) => (
+                <Post post={blog[1]} key={blog[0]} id={blog[0]} blog />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div style={{ textAlign: "center", fontSize: "2rem" }}>First Login</div>
+      )}
+    </>
   );
 }
 
